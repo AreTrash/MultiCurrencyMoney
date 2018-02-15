a36a959c814b11e10bd9add2642a734e38f846de
ShoheiMac <tinpuny44@gmail.com>
2018-02-15 18:07:08 +0900

    fix: virtual修飾子の削除, toStringの削除

--- MultiCurrencyMoney/Money.cs
+++ MultiCurrencyMoney/Money.cs
@@ -9,17 +9,17 @@ namespace MultiCurrencyMoney
         private string currency;
         public string Currency => currency;
 
-        public virtual Money Times(int multiplier)
-        {
-            return new Money(amount * multiplier, Currency);
-        }
-
         public Money(int amount, string currency)
         {
             this.amount = amount;
             this.currency = currency;
         }
-
+        
+        public Money Times(int multiplier)
+        {
+            return new Money(amount * multiplier, Currency);
+        }
+        
         public override bool Equals(object obj)
         {
             var money = (Money)obj;
@@ -34,11 +34,6 @@ namespace MultiCurrencyMoney
         public static Money Franc(int amount)
         {
             return new Money(amount, "CHF");
-        }
-
-        public override string ToString()
-        {
-            return $"{amount} {currency}";
         }
     }
 }
